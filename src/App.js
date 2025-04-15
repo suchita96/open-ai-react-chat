b101f1547260abfd61c7778099fb102412eeef95
suchita96 <suchita.react@gmail.com>
2025-04-15 21:44:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OpenAIChat from './OpenAIChat';
+
+function App() {
+  return (
+    <div className="App">
+      <OpenAIChat />
+    </div>
+  );
+}
+
+export default App;
